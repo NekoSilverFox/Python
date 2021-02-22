06b3482d1ec2ec0763ffbc94081adbbed4c49be7
foxthere <weidufox@gmail.com>
2021-02-22 23:17:59 +0300

    增加 004 - input函数

--- 003 - 变量的使用/main.py
+++ 003 - 变量的使用/main.py
@@ -1,5 +1,5 @@
 # 003 - 变量的使用
-# Python 中变量默认不需要写类型
+# Python 中变量默认不需要指定类型
 qq_num = "123456789"
 
 qq_passwd = "qwe"
@@ -7,3 +7,7 @@ qq_passwd = "qwe"
 print(qq_num)
 
 print(qq_passwd)
+
+# 布尔值：True 或 False
+b = True
+print(b) # True
---
+++ 004 - input函数/main.py
@@ -0,0 +1,7 @@
+# 004 - input函数
+
+var = input("请输入点东西：")
+
+print(type(var))
+
+print(var)
