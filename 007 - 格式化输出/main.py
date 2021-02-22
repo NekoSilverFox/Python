a18e3dceef5159e8f4cf993129293a5e0fbcb40a
foxthere <weidufox@gmail.com>
2021-02-23 00:02:42 +0300

    优化 格式007 - 格式化输出

--- 007 - 格式化输出/main.py
+++ 007 - 格式化输出/main.py
@@ -7,4 +7,10 @@ print("My name is %s, now %d years old" % (name, age))
 print("=" * 100)
 
 student_num = 1
-print("My student numbers is %06d" % student_num) # 000001
+print("My student numbers is %06d" % student_num)  # 000001
+
+print("=" * 100)
+
+print("男女同学各占比 %.2f%%" % 50 * 10)  # 【重点】
+print("---")
+print("男女同学各占比 %.2f%%" % (50 * 10))  # 【重点】
