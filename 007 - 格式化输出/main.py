6648233bf71957400f5d1f02a33b3bb08654e560
foxthere <weidufox@gmail.com>
2021-02-22 23:46:13 +0300

    增加 007 - 格式化输出

---
+++ 007 - 格式化输出/main.py
@@ -0,0 +1,10 @@
+# 007 - 格式化输出
+
+name = "Fox"
+age = 17
+print("My name is %s, now %d years old" % (name, age))
+
+print("=" * 100)
+
+student_num = 1
+print("My student numbers is %06d" % student_num) # 000001
