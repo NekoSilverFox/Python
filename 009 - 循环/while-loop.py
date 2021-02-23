797a5f7161118520b70cf8cbcd7a5ef4c91c6f9c
foxthere <weidufox@gmail.com>
2021-02-23 13:17:00 +0300

    增加 while 循环测试

--- 009 - 循环/while-loop.py
+++ 009 - 循环/while-loop.py
@@ -1,10 +1,18 @@
 # while 循环
 
-int
 i = 1
-
 while i <= 5:
     print("第%d次循环，Hello Python" % i)
     i = i + 1
-
 print("循环结束 i = %d" % i)
+
+print("=" * 50)
+
+result = 0
+i = 0
+while i <= 100:
+    if i % 2 == 0:
+        print(i)
+        result = result + i
+    i += 1
+print("循环结束，100内整数的和 结果为：%d" % result)
