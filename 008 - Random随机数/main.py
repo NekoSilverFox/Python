1f03e23c903718e78560906cf78047a8418fd631
foxthere <weidufox@gmail.com>
2021-02-23 12:57:41 +0300

    增加 print(random.randint(5,10))

---
+++ 008 - Random随机数/main.py
@@ -0,0 +1,11 @@
+# 008 - Random随机数
+
+# 需要导包，同Java
+import random
+
+# 注意：随机数的范围是： [x, y] <--都是闭区间
+print(random.randint(5,10))
+print(random.randint(5,10))
+print(random.randint(5,10))
+print(random.randint(5,10))
+print(random.randint(5,10))
