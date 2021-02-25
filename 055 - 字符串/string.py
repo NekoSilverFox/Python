1d06f2a26ecce0f8d7bce7aaf601974fdd3feb75
foxthere <weidufox@gmail.com>
2021-02-25 11:46:39 +0300

    增加 字符串及其基础操作（判断数字等）

--- 055 - 字符串/string.py
+++ 055 - 字符串/string.py
@@ -21,3 +21,44 @@ print(str.index("Fox"))
 # 判断空白字符，制表符，换行也是空白字符
 str = "     \t    \n"
 print(str.isspace())  # True
+
+print("=" * 50)
+
+# 以下三种方法都不能判断小数
+num_str = "1.1"
+print(num_str)
+print(num_str.isdecimal())  # False 只对字符串是不是全是阿拉伯数字（全角数字）做判断
+print(num_str.isdigit())  # False 可以对全角数字、(1)、\u00b2 做判断
+print(num_str.isnumeric())  # False 还可以对中文数字做判断
+
+print("=" * 50)
+
+num_str = "\u00b2"
+print(num_str)
+print(num_str.isdecimal())  # False 只对字符串是不是全是阿拉伯数字（全角数字）做判断
+print(num_str.isdigit())  # True 可以对全角数字、(1)、\u00b2 做判断
+print(num_str.isnumeric())  # True 还可以对中文数字做判断
+
+print("=" * 50)
+
+num_str = "⑴"  # 注意：是 `⑴` 不是，(1)
+print(num_str)
+print(num_str.isdecimal())  # False 只对字符串是不是全是阿拉伯数字（全角数字）做判断
+print(num_str.isdigit())  # True 可以对全角数字、⑴、\u00b2 做判断
+print(num_str.isnumeric())  # True 还可以对中文数字做判断
+
+print("=" * 50)
+
+num_str = "一千二百"
+print(num_str)
+print(num_str.isdecimal())  # False 只对字符串是不是全是阿拉伯数字（全角数字）做判断
+print(num_str.isdigit())  # False 可以对全角数字、(1)、\u00b2 做判断
+print(num_str.isnumeric())  # True 还可以对中文数字做判断
+
+print("=" * 50)
+
+num_str = "один"
+print(num_str)
+print(num_str.isdecimal())  # False 只对字符串是不是全是阿拉伯数字（全角数字）做判断
+print(num_str.isdigit())  # False 可以对全角数字、(1)、\u00b2 做判断
+print(num_str.isnumeric())  # False 还可以对中文数字做判断
