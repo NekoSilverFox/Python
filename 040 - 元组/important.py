38d602b7a5b0cd8f0a6aa28f079c4e7e6e406f13
foxthere <weidufox@gmail.com>
2021-02-25 09:56:02 +0300

    格式化字符串后面的  本质上就是元组

--- 040 - 元组/important.py
+++ 040 - 元组/important.py
@@ -1,2 +1,9 @@
+# 格式化字符串后面的 `()` 本质上就是元组
 
-# 
+print("%s 的年龄是：%d，身高是：%.2f" % ("Fox", 17, 1.65))
+
+info_tuple = ("Fox", 17, 1.65)
+print("%s 的年龄是：%d，身高是：%.2f" % info_tuple)
+
+info_str = "%s 的年龄是：%d，身高是：%.2f" % info_tuple
+print(info_str)
