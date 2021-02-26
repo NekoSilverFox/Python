e7ca18aee20fc3d6c0b92ff3e65fde2ce1643e59
foxthere <weidufox@gmail.com>
2021-02-26 15:44:17 +0300

    增加 递归案例

--- 029 - 递归/recursion.py
+++ 029 - 递归/recursion.py
@@ -4,9 +4,23 @@ def demo_recursion(num):
     # 递归的出口很重要
     if num == 1:
         return
-    
+
     num -= 1
     demo_recursion(num)
 
 
-demo_recursion(3)
+#demo_recursion(3)
+
+
+# 计算 1 + 2 + 3 + ... + num 的结果
+def sum_numbers(num):
+
+    if num == 1:
+        return 1
+
+    # 数字累加
+    tmp = sum_numbers(num - 1)
+
+    return num + tmp
+
+print(sum_numbers(100))
