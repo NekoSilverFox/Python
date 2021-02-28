3a77d8f8ab99dc562377fcc5f078c4dd6f04e1f7
foxthere <weidufox@gmail.com>
2021-02-28 20:00:34 +0300

    增加 按类型捕获错误

--- 160 - 异常/simpleException.py
+++ 160 - 异常/simpleException.py
@@ -1,7 +1,12 @@
+# 当 `Python` 解释器 **抛出异常** 时，**最后一行错误信息的第一个单词，就是错误类型**
+
 try:
     # 不能确定能正确执行的代码
     num = int(input("请输入一个整数："))
-except:
+    result = 8 / num
+except ZeroDivisionError:
+    print("除零错误！")
+except ValueError:
     # 处理代码
     print("请输入正确的整数")
 
