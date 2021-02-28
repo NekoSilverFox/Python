21624a65c8a6d5b335d69aa32e52bd43f0f2a0d8
foxthere <weidufox@gmail.com>
2021-02-28 20:09:54 +0300

    增加 异常捕获的完整代码

--- 160 - 异常/simpleException.py
+++ 160 - 异常/simpleException.py
@@ -1,9 +1,10 @@
 # 当 `Python` 解释器 **抛出异常** 时，**最后一行错误信息的第一个单词，就是错误类型**
-
 try:
     # 不能确定能正确执行的代码
     num = int(input("请输入一个整数："))
     result = 8 / num
+
+
 except ZeroDivisionError:
     print("除零错误！")
 except ValueError:
@@ -12,4 +13,28 @@ except ValueError:
 # 捕获未知错误     ↓ 注意这里是 `as`
 except Exception as unknown:
     print("发生异常错误： %s" % unknown)
+
+
+else:
+    print("程序没有异常就会执行这句代码")
+
+
+finally:
+    print("无论是否有异常都会执行这句代码")
+
+
 print("-" * 100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
